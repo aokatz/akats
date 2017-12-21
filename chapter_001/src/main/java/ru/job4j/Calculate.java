8fdf80c815879903e67d1ac0a8eecb956bf7eb00
Andrey Kats <andrey.katz@gmail.com>
2017-12-21 19:41:02 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,17 @@
 package ru.job4j;
 
+/**
+ * Класс Calculate. Выводит в консоль привет миру.
+ * @author akats
+ * @version 1
+ * @since 21.12.17
+ */
 public class Calculate
 {
+	/**
+	* Вывод на консоль
+	* @param args аргументы командной строки
+	*/
 	public static void main(String[] args)	
 	{
 		System.out.println("Hello World");
