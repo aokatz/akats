3a32b329d7f5101c21dc22bf8231c99e668ef4e8
Andrey Kats <andrey.katz@gmail.com>
2017-12-23 20:18:33 +0300

    4.4. Построить пирамиду в псевдографике. [#4412]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,11 +1,19 @@
 package ru.job4j.loop;
 
+/**
+ * @author AKats
+ */
 public class Paint {
+    /**
+     * Пирамида в консоли
+     * @param height - высота пирамида
+     * @return - пирамида в String
+     */
     public String pyramid(int height) {
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < height; i++) {
-            for (int j = 0; j < height - 1; j++) {
-                if (j < i) {
+            for (int j = 0; j < height * 2 - 1; j++) {
+                if (i >= height - j - 1 && i + height - 1 >= j) {
                     stringBuilder.append("^");
                 } else {
                     stringBuilder.append(" ");
@@ -14,9 +22,5 @@ public class Paint {
             stringBuilder.append(System.lineSeparator());
         }
         return stringBuilder.toString();
-    }
-
-    public static void main(String[] args) {
-        System.out.println((new Paint()).pyramid(5));
     }
 }
---
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import java.util.StringJoiner;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * @author AKats
+ */
+public class PaintTest {
+
+    @Test
+    public void whenPyramid4Right() {
+        Paint paint = new Paint();
+        String rst = paint.pyramid(4);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^   ")
+                                .add("  ^^^  ")
+                                .add(" ^^^^^ ")
+                                .add("^^^^^^^")
+                                .toString()
+                )
+        );
+    }
+
+}
