c584ac28ce5253f85126bde740dc7cfed4c018bd
Andrey Kats <andrey.katz@gmail.com>
2017-12-25 13:15:41 +0300

    4.5. Рефакторинг кода. [#33459]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,26 +1,35 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
+
 /**
  * @author AKats
  */
 public class Paint {
+
+        public String loopBy(int height, int weight, BiPredicate<Integer, Integer> predict) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < weight; j++) {
+                    if (predict.test(i, j)) {
+                        stringBuilder.append("^");
+                    } else {
+                        stringBuilder.append(" ");
+                    }
+                }
+                stringBuilder.append(System.lineSeparator());
+            }
+            return stringBuilder.toString();
+       }
     /**
      * Пирамида в консоли
      * @param height - высота пирамида
      * @return - пирамида в String
      */
-    public String pyramid(int height) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < height * 2 - 1; j++) {
-                if (i >= height - j - 1 && i + height - 1 >= j) {
-                    stringBuilder.append("^");
-                } else {
-                    stringBuilder.append(" ");
-                }
-            }
-            stringBuilder.append(System.lineSeparator());
-        }
-        return stringBuilder.toString();
+       public String pyramid(int height) {
+            return this.loopBy(
+                    height,
+                    2 * height - 1,
+                    (i, j) -> i >= height - j - 1 && i + height - 1 >= j);
+       }
     }
-}
