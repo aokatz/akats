20bbf8c990ae3ae95693adb2b446636bd060bfa2
aokatz <andrey.katz@gmail.com>
2018-01-29 18:54:44 +0300

    5.2.1. Реализовать SimpleArray<T>

---
+++ chapter_004/src/main/java/ru/job4j/iterators/SimpleArray.java
@@ -0,0 +1,73 @@
+package ru.job4j.iterators;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+
+/**
+ * Simple array of T type elements
+ *
+ * @param <T> - type of elements
+ * @author AKats
+ */
+public class SimpleArray<T> implements Iterable<T> {
+    private int index = 0;
+    private Object[] array;
+
+    public SimpleArray() {
+        array = new Object[0];
+    }
+
+    public Object[] getArray() {
+        return array;
+    }
+
+    private void checkArrayLength() {
+        if (this.index + 1 > this.array.length) {
+            this.array = Arrays.copyOf(this.array, this.array.length + 1);
+        }
+    }
+
+    public void add(T model) {
+        checkArrayLength();
+        this.array[index++] = model;
+    }
+
+    public void set(int index, T model) {
+        this.array[index] = model;
+    }
+
+    public void delete(int index) {
+        Object[] res = new Object[this.array.length - 1];
+        this.index--;
+        for (int i = 0, j = 0; j < res.length; i++, j++) {
+            if (i == index) {
+                ++i;
+            }
+            res[j] = this.array[i];
+        }
+        this.array = res;
+    }
+
+    public T get(int index) {
+        return (T) this.array[index];
+    }
+
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int current = 0;
+
+            @Override
+            public boolean hasNext() {
+                return current < index;
+            }
+
+            @Override
+            public T next() {
+                return (T) array[current++];
+            }
+        };
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/iterators/SimpleArrayTest.java
@@ -0,0 +1,52 @@
+package ru.job4j.iterators;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class SimpleArrayTest {
+    private SimpleArray<Integer> simpleArray;
+
+    @Before
+    public void setUp() {
+        simpleArray = new SimpleArray<>();
+        simpleArray.add(1);
+        simpleArray.add(2);
+        simpleArray.add(3);
+        simpleArray.add(4);
+        simpleArray.add(5);
+    }
+
+    @Test
+    public void whenAddElementShouldAdd() {
+        assertThat(simpleArray.getArray(), is(new Integer[]{1, 2, 3, 4, 5}));
+    }
+
+    @Test
+    public void whenSetElementShouldReplaceIt() {
+        simpleArray.set(3, 10);
+        assertThat(simpleArray.getArray(), is(new Integer[]{1, 2, 3, 10, 5}));
+    }
+
+    @Test
+    public void whenDeleteShouldDelete() {
+        simpleArray.delete(3);
+        assertThat(simpleArray.getArray(), is(new Integer[]{1, 2, 3, 5}));
+    }
+
+    @Test
+    public void whenGetElementShouldGetIt() {
+        assertThat(simpleArray.get(0), is(1));
+    }
+
+    @Test
+    public void shouldReturnElementsUsingIterator() {
+        int i = 0;
+        for (Integer elem: simpleArray
+             ) {
+            assertThat(elem, is(simpleArray.get(i++)));
+        }
+    }
+}
