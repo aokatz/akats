9f77f2a1786e0b4914e0a9af5ede4386713746fe
aokatz <andrey.katz@gmail.com>
2018-02-01 12:26:25 +0300

    1. Создать модель User

---
+++ chapter_004/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,15 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+/**
+ * Class User
+ *
+ * @author AKats
+ */
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+}
