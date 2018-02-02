f9f411dac5e627e049a12b1b4f6337b6431907b1
aokatz <andrey.katz@gmail.com>
2018-02-02 10:35:43 +0300

    2. Не перекрывать equals hashCode

--- chapter_004/src/main/java/ru/job4j/map/User.java
+++ chapter_004/src/main/java/ru/job4j/map/User.java
@@ -12,4 +12,18 @@ public class User {
     private int children;
     private Calendar birthday;
 
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", children=" + children +
+                ", birthday=" + birthday.get(1) +
+                '}';
+    }
 }
