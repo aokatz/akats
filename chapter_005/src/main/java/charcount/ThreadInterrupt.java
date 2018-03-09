21f55f595f3f6031f138657530b933b5935c4659
aokatz <andrey.katz@gmail.com>
2018-03-10 00:23:33 +0300

    3. Реализовать механизм программнной остановки потока.

---
+++ chapter_005/src/main/java/charcount/ThreadInterrupt.java
@@ -0,0 +1,50 @@
+package charcount;
+
+public class ThreadInterrupt {
+    int nanoseconds;
+    String text;
+    private volatile boolean isRunning = true;
+
+    public ThreadInterrupt(int nanoseconds, String text) {
+        this.nanoseconds = nanoseconds;
+        this.text = text;
+    }
+
+    public class CountChar implements Runnable {
+        charcount.CountChar countChar = new charcount.CountChar(text);
+        @Override
+        public void run() {
+            while (isRunning) {
+                countChar.count();
+                break;
+            }
+        }
+    }
+
+    public class Time implements Runnable {
+        long currTime;
+
+        @Override
+        public void run() {
+            currTime = System.nanoTime();
+            long elapsedTime = System.nanoTime() - currTime;
+            while (elapsedTime < nanoseconds) {
+                elapsedTime = System.nanoTime() - currTime;
+            }
+            isRunning = false;
+        }
+    }
+
+    public void count() {
+        Thread timeThread = new Thread(new Time());
+        Thread countThread = new Thread(new CountChar());
+        timeThread.start();
+        countThread.start();
+    }
+
+    public static void main(String[] args) {
+        ThreadInterrupt threadInterrupt = new ThreadInterrupt(1, "sdfsdf sdfs 123");
+        threadInterrupt.count();
+    }
+
+}
