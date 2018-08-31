bd9935fdceea5c10ce3559bdbd41ac970f3bc55d
aokatz <andrey.katz@gmail.com>
2018-08-31 13:38:13 +0300

    1. Реализовать шаблон Producer Consumer. [#1098]

---
+++ chapter_005/src/main/java/prodcons/SimpleBlockingQueue.java
@@ -0,0 +1,42 @@
+package prodcons;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+
+    @GuardedBy("this")
+    private Queue<T> queue = new LinkedList<>();
+    private final Object lock = new Object();
+    private final int LIMIT = 3;
+
+    public void offer(T value) {
+        synchronized (this.lock) {
+            while (queue.size() == LIMIT) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            queue.add(value);
+            lock.notifyAll();
+        }
+    }
+
+    public T poll() throws InterruptedException {
+        T retvalue;
+        synchronized (this.lock) {
+            while (queue.isEmpty()) {
+                lock.wait();
+            }
+            retvalue = queue.poll();
+            lock.notifyAll();
+            return retvalue;
+        }
+    }
+}
