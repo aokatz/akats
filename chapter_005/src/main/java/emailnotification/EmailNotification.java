cc85ed61e3a1bdd80a35ab75758e8e6072263078
aokatz <andrey.katz@gmail.com>
2018-11-01 15:35:57 +0300

    2. ExecutorService рассылка почты. [#63097]

---
+++ chapter_005/src/main/java/emailnotification/EmailNotification.java
@@ -0,0 +1,22 @@
+package emailnotification;
+
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+    ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    public void close() {
+        pool.shutdown();
+    }
+
+    public void emailTo(User user) {
+        pool.submit(() -> send("Notification " + user.getUsername() + " to email " + user.getEmail() + ".", "Add a new event to " + user.getUsername(), user.getEmail()));
+    }
+
+    public void send(String subject, String body, String email) {
+
+    }
+
+}
