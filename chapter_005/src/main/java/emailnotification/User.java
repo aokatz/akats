bb2a9edf66e1c6853837f5ba7df65cd86eba187b
aokatz <andrey.katz@gmail.com>
2018-12-11 18:40:24 +0300

    2. ExecutorService рассылка почты. [#63097]

--- chapter_005/src/main/java/emailnotification/User.java
+++ chapter_005/src/main/java/emailnotification/User.java
@@ -1,23 +1,15 @@
 package emailnotification;
 
 public class User {
-    private String username;
-    private String email;
+    private final String username;
+    private final String email;
 
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public User(String username, String email) {
